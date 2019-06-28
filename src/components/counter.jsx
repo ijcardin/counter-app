9145fd25158ccfed7f89dfcb162930afe6c47d89
Ian Cardinoza <ijcardin@ucalgary.ca>
2019-06-27 19:32:16 -0600

    completed exercise

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,47 +1,42 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-
-  state = {
-      value: this.props.value,
-  };
-// Only use if the arrow operator doesnt work
-//   constructor() {
-//       super();
-//       this.handleIncrement = this.handleIncrement.bind(this);
-//   }
-
-  handleIncrement = () => {
-      this.setState({value: this.state.value + 1});
-  };
-
+  // Only use if the arrow operator doesnt work
+  //   constructor() {
+  //       super();
+  //       this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
   render() {
     return (
       <div>
+        {/* <h4>Counter #{this.props.id}</h4> */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button 
-            onClick={this.handleIncrement} 
-            className="btn btn-secondary btn-sm"
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
         >
-            Increment
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.value === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount() {
-        const { value: count } = this.state;
-        return count === 0 ? "Zero" : count;
-    }
+  formatCount() {
+    const { value: count } = this.props.counter;
+    return count === 0 ? "Zero" : count;
+  }
 }
-
-
 
 export default Counter;
